b86d8a7fd7f0187dd913cdd49c2f01d52b1d5b1a
Marcin <cinula@gmail.com>
2024-04-21 12:16:02 +0100

    add js function for game to work and scores displey

---
+++ assets/script.js
@@ -0,0 +1,58 @@
+const choice = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+const playerView = document.getElementById("player");
+const comView = document.getElementById("com");
+const resultsView = document.getElementById("results");
+const playerScoreView = document.getElementById('playerScore');
+const computerScoreView = document.getElementById('computerScore');
+let playerScore = 0;
+let computerScore = 0;
+
+function game(playerChoice) {
+    const computerChoice = choice[Math.floor(Math.random() * 5)];
+    let results = "";
+
+    if (playerChoice === computerChoice) {
+        results = "It's a Draw!";
+    } else {
+        switch (playerChoice) {
+            case "Rock":
+                results = (computerChoice === "Scissors") ? "You Win!" : "You Lose!";
+                results = (computerChoice === "Lizard") ? "You Win!" : "You Lose!";
+                break;
+            case "Paper":
+                results = (computerChoice === "Rock") ? "You Win!" : "You Lose!";
+                results = (computerChoice === "Spock") ? "You Win!" : "You Lose!";
+                break;
+            case "Scissors":
+                results = (computerChoice === "Paper") ? "You Win!" : "You Lose!";
+                results = (computerChoice === "Lizard") ? "You Win!" : "You Lose!";
+                break;
+            case "Lizard":
+                results = (computerChoice === "Paper") ? "You Win!" : "You Lose!";
+                results = (computerChoice === "Spock") ? "You Win!" : "You Lose!";
+                break;
+            case "Spock":
+                results = (computerChoice === "Scissors") ? "You Win!" : "You Lose!";
+                results = (computerChoice === "Rock") ? "You Win!" : "You Lose!";
+                break;
+        }
+    }
+    playerView.textContent = `Player: ${playerChoice}`;
+    comView.textContent = `Computer: ${computerChoice}`;
+    resultsView.textContent = results;
+
+    resultsDisplay.classList.remove("greenText", "redText");
+
+    switch (results) {
+        case "You Win!":
+            resultsView.classList.add("greenText");
+            playerScore++;
+            playerScoreView.textContent = playerScore;
+            break;
+        case "You Lose!":
+            resultsView.classList.add("redText");
+            computerScore++;
+            computerScoreView.textContent = computerScore;
+            break;
+    }
+}
